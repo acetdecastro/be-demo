fac16c0fed39b50e24710fbff42dc823e5af0e0f
Ace De Castro <acetdecastro@gmail.com>
2024-11-04 21:23:54 +0800

    Add GqlAuthGuard in Blockchain Resolver (#9)

--- src/blockchain/blockchain.resolver.ts
+++ src/blockchain/blockchain.resolver.ts
@@ -1,11 +1,14 @@
 import { Resolver, Query } from '@nestjs/graphql';
 import { BlockchainService } from './blockchain.service';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth-guard';
 
 @Resolver()
 export class BlockchainResolver {
   constructor(private readonly blockchainService: BlockchainService) {}
 
   @Query(() => String, { name: 'getMarketplaceManager' })
+  @UseGuards(GqlAuthGuard)
   async getMarketplaceManager(): Promise<string> {
     return await this.blockchainService.getMarketplaceManager();
   }
